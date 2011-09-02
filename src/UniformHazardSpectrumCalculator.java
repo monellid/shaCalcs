d198925d76dfad93dbe1dfb6f6888e333b4059b3
Damiano Monelli <monelli.damiano@gmail.com>
2011-09-02 18:00:11 +0200

    small refactoring

--- src/UniformHazardSpectrumCalculator.java
+++ src/UniformHazardSpectrumCalculator.java
@@ -106,119 +106,25 @@ public class UniformHazardSpectrumCalculator {
 
 					// if period==0.0, this is PGA
 					if (period == 0.0) {
-						imr.setIntensityMeasure(PGA_Param.NAME);
-						// loop over intensity measure levels
-						for (int i = 0; i < imlVals.size(); i++) {
-							imr.setIntensityMeasureLevel(imlVals.get(i));
-							double probExc = imr.getExceedProbability();
-							double hazCurveValue = hazCurvesPerPeriod.get(
-									period).getY(i);
-							hazCurvesPerPeriod.get(period).set(
-									i,
-									hazCurveValue
-											* Math.pow(1 - rupProb, probExc));
-						}
+						computeHazardCurveForPGA(hazCurvesPerPeriod, imr,
+								rupProb, period);
 					}
 					// in case the period is equal to one of the supported
 					// periods
 					else if (periodParam.isAllowed(period)) {
-						imr.setIntensityMeasure(SA_Param.NAME);
-						imr.getParameter(PeriodParam.NAME).setValue(period);
-
-						// loop over intensity measure levels
-						for (int i = 0; i < imlVals.size(); i++) {
-							imr.setIntensityMeasureLevel(imlVals.get(i));
-							double probExc = imr.getExceedProbability();
-							double hazCurveValue = hazCurvesPerPeriod.get(
-									period).getY(i);
-							hazCurvesPerPeriod.get(period).set(
-									i,
-									hazCurveValue
-											* Math.pow(1 - rupProb, probExc));
-						}
+						computeHazardCurveForSA(hazCurvesPerPeriod, imr,
+								rupProb, period);
 
 					} // if it is between PGA and the first SA
 					else if (period > 0.0 && period < periodList.get(0)) {
-						
-						double period1 = 0.0;
-						double period2 = periodList.get(0);
-						
-						// loop over intensity measure levels
-						for (int i = 0; i < imlVals.size(); i++) {
-							imr.setIntensityMeasureLevel(imlVals.get(i));
-
-							// compute probabilities of exceedence for the two
-							// closest periods
-							imr.setIntensityMeasure(PGA_Param.NAME);
-							double probExc1 = imr.getExceedProbability();
-							imr.setIntensityMeasure(SA_Param.NAME);
-							imr.getParameter(PeriodParam.NAME)
-									.setValue(period2);
-							double probExc2 = imr.getExceedProbability();
-
-							// interpolate
-							// compute interpolation coefficients. Follow
-							// equations
-							// 3.3.1 and 3.3.2, pag. 107 in "Numerical Recipes
-							// in
-							// Fortran 77"
-							double A = (period2 - period) / (period2 - period1);
-							double B = 1 - A;
-							double probExc = A * probExc1 + B * probExc2;
-
-							double hazCurveValue = hazCurvesPerPeriod.get(
-									period).getY(i);
-							hazCurvesPerPeriod.get(period).set(
-									i,
-									hazCurveValue
-											* Math.pow(1 - rupProb, probExc));
-						}
+						computeHazardCurveForPeriodBetweenPGAandSA(
+								hazCurvesPerPeriod, imr, periodList, rupProb,
+								period);
 
 					} else {
-						// find the two closest periods and compute the
-						// probability of exceedence by interpolating the
-						// probabilities of exceedence of the closest periods
-						int ii;
-						for (ii = 0; ii < periodList.size(); ii++) {
-							if (period > periodList.get(ii)
-									&& period < periodList.get(ii + 1)) {
-								break;
-							}
-						}
-						double period1 = periodList.get(ii);
-						double period2 = periodList.get(ii + 1);
-						imr.setIntensityMeasure(SA_Param.NAME);
-						
-						// loop over intensity measure levels
-						for (int i = 0; i < imlVals.size(); i++) {
-							imr.setIntensityMeasureLevel(imlVals.get(i));
-
-							// compute probabilities of exceedence for the two
-							// closest periods
-							imr.getParameter(PeriodParam.NAME)
-									.setValue(period1);
-							double probExc1 = imr.getExceedProbability();
-							imr.getParameter(PeriodParam.NAME)
-									.setValue(period2);
-							double probExc2 = imr.getExceedProbability();
-
-							// interpolate
-							// compute interpolation coefficients. Follow
-							// equations
-							// 3.3.1 and 3.3.2, pag. 107 in "Numerical Recipes
-							// in
-							// Fortran 77"
-							double A = (period2 - period) / (period2 - period1);
-							double B = 1 - A;
-							double probExc = A * probExc1 + B * probExc2;
-
-							double hazCurveValue = hazCurvesPerPeriod.get(
-									period).getY(i);
-							hazCurvesPerPeriod.get(period).set(
-									i,
-									hazCurveValue
-											* Math.pow(1 - rupProb, probExc));
-						}
+						computeHazardCurveForInterpolatedPeriod(
+								hazCurvesPerPeriod, imr, periodList, rupProb,
+								period);
 
 					}
 				}
@@ -228,6 +134,134 @@ public class UniformHazardSpectrumCalculator {
 		}
 
 		return uhs;
+	}
+
+	private void computeHazardCurveForInterpolatedPeriod(
+			Map<Double, DiscretizedFuncAPI> hazCurvesPerPeriod,
+			ScalarIntensityMeasureRelationshipAPI imr,
+			ArrayList<Double> periodList, double rupProb, double period) {
+		
+		// find the two closest periods and compute the
+		// probability of exceedence by interpolating the
+		// probabilities of exceedence of the closest periods
+		int ii = getPeriodIndex(periodList, period);
+		double period1 = periodList.get(ii);
+		double period2 = periodList.get(ii + 1);
+		imr.setIntensityMeasure(SA_Param.NAME);
+
+		// loop over intensity measure levels
+		for (int i = 0; i < imlVals.size(); i++) {
+			imr.setIntensityMeasureLevel(imlVals.get(i));
+
+			// compute probabilities of exceedence for the two
+			// closest periods
+			imr.getParameter(PeriodParam.NAME)
+					.setValue(period1);
+			double probExc1 = imr.getExceedProbability();
+			imr.getParameter(PeriodParam.NAME)
+					.setValue(period2);
+			double probExc2 = imr.getExceedProbability();
+
+			double probExc = getInterpolatedProb(period,
+					period1, period2, probExc1, probExc2);
+
+			computeHazardCurve(hazCurvesPerPeriod, rupProb, period, i, probExc);
+		}
+	}
+
+	private void computeHazardCurve(
+			Map<Double, DiscretizedFuncAPI> hazCurvesPerPeriod, double rupProb,
+			double period, int i, double probExc) {
+		double hazCurveValue = hazCurvesPerPeriod.get(
+				period).getY(i);
+		hazCurvesPerPeriod.get(period).set(
+				i,
+				hazCurveValue
+						* Math.pow(1 - rupProb, probExc));
+	}
+
+	private int getPeriodIndex(ArrayList<Double> periodList, double period) {
+		int ii;
+		for (ii = 0; ii < periodList.size(); ii++) {
+			if (period > periodList.get(ii)
+					&& period < periodList.get(ii + 1)) {
+				break;
+			}
+		}
+		return ii;
+	}
+
+	private void computeHazardCurveForPeriodBetweenPGAandSA(
+			Map<Double, DiscretizedFuncAPI> hazCurvesPerPeriod,
+			ScalarIntensityMeasureRelationshipAPI imr,
+			ArrayList<Double> periodList, double rupProb, double period) {
+
+		double period1 = 0.0;
+		double period2 = periodList.get(0);
+
+		// loop over intensity measure levels
+		for (int i = 0; i < imlVals.size(); i++) {
+			imr.setIntensityMeasureLevel(imlVals.get(i));
+
+			// compute probabilities of exceedence for PGA and first SA period
+			imr.setIntensityMeasure(PGA_Param.NAME);
+			double probExc1 = imr.getExceedProbability();
+			imr.setIntensityMeasure(SA_Param.NAME);
+			imr.getParameter(PeriodParam.NAME).setValue(period2);
+			double probExc2 = imr.getExceedProbability();
+
+			double probExc = getInterpolatedProb(period, period1, period2,
+					probExc1, probExc2);
+
+			computeHazardCurve(hazCurvesPerPeriod, rupProb, period, i, probExc);
+		}
+	}
+
+	private void computeHazardCurveForSA(
+			Map<Double, DiscretizedFuncAPI> hazCurvesPerPeriod,
+			ScalarIntensityMeasureRelationshipAPI imr, double rupProb,
+			double period) {
+
+		imr.setIntensityMeasure(SA_Param.NAME);
+		imr.getParameter(PeriodParam.NAME).setValue(period);
+
+		// loop over intensity measure levels
+		for (int i = 0; i < imlVals.size(); i++) {
+			imr.setIntensityMeasureLevel(imlVals.get(i));
+			double probExc = imr.getExceedProbability();
+			computeHazardCurve(hazCurvesPerPeriod, rupProb, period, i, probExc);
+		}
+	}
+
+	private void computeHazardCurveForPGA(
+			Map<Double, DiscretizedFuncAPI> hazCurvesPerPeriod,
+			ScalarIntensityMeasureRelationshipAPI imr, double rupProb,
+			double period) {
+
+		imr.setIntensityMeasure(PGA_Param.NAME);
+
+		// loop over intensity measure levels
+		for (int i = 0; i < imlVals.size(); i++) {
+			imr.setIntensityMeasureLevel(imlVals.get(i));
+			double probExc = imr.getExceedProbability();
+			computeHazardCurve(hazCurvesPerPeriod, rupProb, period, i, probExc);
+		}
+	}
+
+	// compute poE for period, given poEs for period1 and period2. Use linear
+	// interpolation equation
+	private double getInterpolatedProb(double period, double period1,
+			double period2, double probExc1, double probExc2) {
+		// interpolate
+		// compute interpolation coefficients. Follow
+		// equations
+		// 3.3.1 and 3.3.2, pag. 107 in "Numerical Recipes
+		// in
+		// Fortran 77"
+		double A = (period2 - period) / (period2 - period1);
+		double B = 1 - A;
+		double probExc = A * probExc1 + B * probExc2;
+		return probExc;
 	}
 
 	// check that the specified GMPEs provide spectral accelerations
