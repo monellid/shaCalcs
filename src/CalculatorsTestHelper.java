0682b59c700dc0d677f342b91ae7569226969ee2
Damiano Monelli <monelli.damiano@gmail.com>
2011-09-05 15:45:15 +0200

    updated tests

--- src/CalculatorsTestHelper.java
+++ src/CalculatorsTestHelper.java
@@ -17,6 +17,7 @@ import org.opensha.sha.earthquake.rupForecastImpl.GEM1.SourceData.GEMAreaSourceD
 import org.opensha.sha.earthquake.rupForecastImpl.GEM1.SourceData.GEMSourceData;
 import org.opensha.sha.imr.ScalarIntensityMeasureRelationshipAPI;
 import org.opensha.sha.imr.attenRelImpl.BA_2008_AttenRel;
+import org.opensha.sha.imr.attenRelImpl.CB_2008_AttenRel;
 import org.opensha.sha.imr.param.IntensityMeasureParams.PGA_Param;
 import org.opensha.sha.imr.param.IntensityMeasureParams.PeriodParam;
 import org.opensha.sha.imr.param.IntensityMeasureParams.SA_Param;
@@ -24,6 +25,7 @@ import org.opensha.sha.imr.param.OtherParams.ComponentParam;
 import org.opensha.sha.imr.param.OtherParams.SigmaTruncLevelParam;
 import org.opensha.sha.imr.param.OtherParams.SigmaTruncTypeParam;
 import org.opensha.sha.imr.param.OtherParams.StdDevTypeParam;
+import org.opensha.sha.imr.param.SiteParams.DepthTo2pt5kmPerSecParam;
 import org.opensha.sha.imr.param.SiteParams.Vs30_Param;
 import org.opensha.sha.magdist.GutenbergRichterMagFreqDist;
 import org.opensha.sha.util.TectonicRegionType;
@@ -55,8 +57,21 @@ public class CalculatorsTestHelper {
 		return imlVals;
 	}
 
-	public static ScalarIntensityMeasureRelationshipAPI getTestIMR() {
+	public static ScalarIntensityMeasureRelationshipAPI getTestIMRActiveShallow() {
 		ScalarIntensityMeasureRelationshipAPI imr = new BA_2008_AttenRel(null);
+		imr.setIntensityMeasure(PGA_Param.NAME);
+		imr.getParameter(StdDevTypeParam.NAME).setValue(
+				StdDevTypeParam.STD_DEV_TYPE_TOTAL);
+		imr.getParameter(SigmaTruncTypeParam.NAME).setValue(
+				SigmaTruncTypeParam.SIGMA_TRUNC_TYPE_2SIDED);
+		imr.getParameter(SigmaTruncLevelParam.NAME).setValue(3.0);
+		imr.getParameter(ComponentParam.NAME).setValue(
+				ComponentParam.COMPONENT_GMRotI50);
+		return imr;
+	}
+	
+	public static ScalarIntensityMeasureRelationshipAPI getTestIMRStableContinental() {
+		ScalarIntensityMeasureRelationshipAPI imr = new CB_2008_AttenRel(null);
 		imr.setIntensityMeasure(PGA_Param.NAME);
 		imr.getParameter(StdDevTypeParam.NAME).setValue(
 				StdDevTypeParam.STD_DEV_TYPE_TOTAL);
@@ -69,7 +84,7 @@ public class CalculatorsTestHelper {
 	}
 
 	public static double[] getTestIMRPeriods() {
-		SA_Param sa = (SA_Param) CalculatorsTestHelper.getTestIMR()
+		SA_Param sa = (SA_Param) CalculatorsTestHelper.getTestIMRActiveShallow()
 				.getParameter(SA_Param.NAME);
 		PeriodParam periodParam = sa.getPeriodParam();
 		ArrayList<Double> periodValues = periodParam.getAllowedDoubles();
@@ -82,19 +97,21 @@ public class CalculatorsTestHelper {
 
 	public static Map<TectonicRegionType, ScalarIntensityMeasureRelationshipAPI> getTestImrMap() {
 		Map<TectonicRegionType, ScalarIntensityMeasureRelationshipAPI> imrMap = new HashMap<TectonicRegionType, ScalarIntensityMeasureRelationshipAPI>();
-		imrMap.put(TectonicRegionType.ACTIVE_SHALLOW, getTestIMR());
+		imrMap.put(TectonicRegionType.ACTIVE_SHALLOW, getTestIMRActiveShallow());
+		imrMap.put(TectonicRegionType.STABLE_SHALLOW, getTestIMRStableContinental());
 		return imrMap;
 	}
 
 	public static GEM1ERF getTestERF() {
 
 		ArrayList<GEMSourceData> srcList = new ArrayList<GEMSourceData>();
-		srcList.add(getTestSourceData());
+		srcList.add(getTestSourceDataActiveShallow());
+		srcList.add(getTestSourceDataStableCrust());
 		double timeSpan = 50.0;
 		return GEM1ERF.getGEM1ERF(srcList, timeSpan);
 	}
 
-	public static GEMSourceData getTestSourceData() {
+	public static GEMSourceData getTestSourceDataActiveShallow() {
 
 		String id = "src1";
 		String name = "testSource";
@@ -128,9 +145,46 @@ public class CalculatorsTestHelper {
 		return srcData;
 	}
 	
+	public static GEMSourceData getTestSourceDataStableCrust() {
+
+		String id = "src1";
+		String name = "testSource";
+		TectonicRegionType tectReg = TectonicRegionType.STABLE_SHALLOW;
+		LocationList border = new LocationList();
+		border.add(new Location(-0.5, 0.5));
+		border.add(new Location(0.5, 0.5));
+		border.add(new Location(0.5, 1.5));
+		border.add(new Location(-0.5, 1.5));
+		Region reg = new Region(border, BorderType.GREAT_CIRCLE);
+		double bValue = 1.0;
+		double totCumRate = 0.2;
+		double min = 5.05;
+		double max = 8.95;
+		int num = 41;
+		GutenbergRichterMagFreqDist magDist = new GutenbergRichterMagFreqDist(
+				bValue, totCumRate, min, max, num);
+		double strike = 0.0;
+		double dip = 90.0;
+		double rake = 0.0;
+		FocalMechanism focalMechanism = new FocalMechanism(strike, dip, rake);
+		MagFreqDistsForFocalMechs magfreqDistFocMech = new MagFreqDistsForFocalMechs(
+				magDist, focalMechanism);
+		ArbitrarilyDiscretizedFunc aveRupTopVsMag = new ArbitrarilyDiscretizedFunc();
+		double magThreshold = 6.5;
+		double topOfRuptureDepth = 0.0;
+		aveRupTopVsMag.set(magThreshold, topOfRuptureDepth);
+		double aveHypoDepth = 5.0;
+		GEMSourceData srcData = new GEMAreaSourceData(id, name, tectReg, reg,
+				magfreqDistFocMech, aveRupTopVsMag, aveHypoDepth);
+		return srcData;
+	}
+	
+	
+	
 	public static Site getTestSite(){
 		Site site = new Site(new Location(0.0, 0.0));
 		site.addParameter(new DoubleParameter(Vs30_Param.NAME, 760.0));
+		site.addParameter(new DoubleParameter(DepthTo2pt5kmPerSecParam.NAME, 1.0));
 		return site;
 	}
 	
